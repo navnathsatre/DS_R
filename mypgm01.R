9c52f4f2008959599792dec4a91437dc1cadfa7a
navnathsatre <75266852+navnathsatre@users.noreply.github.com>
2020-12-17 08:55:48 +0530

    Add files via upload

---
+++ mypgm01.R
@@ -0,0 +1,43 @@
+getwd()
+#setwd("F:\\R.mypgm1")
+
+# my first pgm in R
+
+2+5
+
+
+5^6
+exp(2)
+
+
+var1<-5
+2>6
+3*5+6
+exp1
+exp(1)
+sqrt(2.6)
+pi
+pi
+x<-5
+x<-3
+y<-6
+z<-8
+x+y+z
+# R is case sensitive language
+6*pi-688
+2*pi*6378 # circomferance of earth at equator 
+
+This.Year
+This.Year<-2020
+nath <- c(1,2,3,4)
+nath
+vec <- c(10,20,30,40)
+vec
+vec1<-c("R","Python","java")
+vec1<-c('c',"C++","C#")
+var2<-c(10,"S",25,'java')
+var2
+nath[3]
+nath[]
+
+getwd()
